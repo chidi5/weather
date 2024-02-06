c0457063ef1db2100ce847e863746cba8dcb4a48
chidi5 <45231284+chidi5@users.noreply.github.com>
2024-02-06 20:22:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
-    <div className="centered md:h-fit">
+    <div className="centered h-screen md:h-fit">
       <HomeScreen />
     </div>
   );
